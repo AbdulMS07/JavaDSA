40baa914f48b98d485fb17bbc2690ac668d38b1e
AbdulMS07 <abdulafzalalim@gmail.com>
2021-04-08 15:58:19 +0530

    fir

---
+++ BinarySearchRecursive.java
@@ -0,0 +1,49 @@
+//code By M.Shaihu Abdul Kadhir
+
+import java.util.*;
+
+public class BinarySearchRecursive{
+    public static void main (String[] args) {
+        
+        BinarySearchRecursive bs = new BinarySearchRecursive();
+        Scanner scan = new Scanner(System.in);	
+        
+        System.out.println("Enter the size of the array: ");
+        int n=scan.nextInt();
+        int arr[] = new int[n];
+        
+        System.out.println("Enter the array elements in sorted: ");
+        for(int i=0;i<n;i++){
+            arr[i]=scan.nextInt();
+        }
+        
+        System.out.println("Enter the element to find: ");
+        int k=scan.nextInt();
+        
+        int result,first=0;
+        result=bs.binarySearch(arr,first,n-1,k);
+        if(result==-1){
+             System.out.println("There is no such element");
+        }else{
+             System.out.println("The key element "+k+" is found at index "+result);
+        }
+       
+        scan.close();
+    }
+    public int binarySearch(int arr[],int first,int last,int key){
+        if(last>=first){
+            int mid = (int)first+(last-first)/2;
+            if(key==arr[mid]){
+                return mid;
+            }
+            if(key<arr[mid]){
+                return binarySearch(arr,first,mid-1,key);
+            }
+            if(key>arr[mid]){
+                return binarySearch(arr,mid+1,last,key);
+            }
+        }
+       return -1;
+    }
+	
+}
